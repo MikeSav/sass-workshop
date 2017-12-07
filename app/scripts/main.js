d99e9440adc9430d32506a8a643223cbaba96156
Mike Savage <michael.savage@msasafety.com>
2017-12-07 16:09:29 +0100

    Original Branch and start of exercise

---
+++ app/scripts/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// so odd doing this without Angular!
+function toggleMenu() {
+  if (document.querySelector('.content').classList.contains('content-closed')) {
+    document.querySelector('.content').classList.remove('content-closed');
+  } else {
+    document.querySelector('.content').classList.add('content-closed');
+  }
+}
