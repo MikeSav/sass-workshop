21718e637f4846d234c71d77a7d08cab2f180260
Mike Savage <michael.savage@msasafety.com>
2018-06-15 17:41:41 +0200

    Converted css to BEM

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,9 +2,9 @@
 
 // so odd doing this without Angular!
 function toggleMenu() {
-  if (document.querySelector('.content').classList.contains('content-closed')) {
-    document.querySelector('.content').classList.remove('content-closed');
+  if (document.querySelector('.content').classList.contains('content--closed')) {
+    document.querySelector('.content').classList.remove('content--closed');
   } else {
-    document.querySelector('.content').classList.add('content-closed');
+    document.querySelector('.content').classList.add('content--closed');
   }
 }
